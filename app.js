5161207ca2cf81c95b53cc49bab8d0ea5ef2ac63
AlondraOrtiz656 <alondraortiz656.com>
2025-08-27 15:51:49 -0400

    Primer avance, arreglo funcional

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    amigoUsuario = document.getElementById("amigo").value.trim();  
+    if (amigoUsuario == ""){
+        alert("Debe insertar un amigo para añadirlo en la lista");
+        return;
+    }
+    amigos.push(amigoUsuario);  
+    console.log(amigos);    
+    limpiarCaja();
+}
+    
+function limpiarCaja(){
+    document.getElementById("amigo").value="";
+}
